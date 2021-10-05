dfb16dfdca262eb13234eebcb326853b4e2cefb8
Naaidrv <zabriverav@gmail.com>
2021-10-05 13:49:06 -0500

    Más preguntas, más divertidas :D

--- lib/data/question_list.dart
+++ lib/data/question_list.dart
@@ -2,30 +2,181 @@ import 'package:quizapp/model/question_model.dart';
 
 List<QuestionModel> questions = [
   QuestionModel(
-    "¿Cuántos años tengo?",
+    "¿Cuáles son los cinco tipos de sabores primarios?",
     {
-      "22": false,
-      "19": false,
-      "25": false,
-      "24": true,
+      "Salado y dulce": false,
+      "Amargo y ácido": false,
+      "Umami, dulce, ácido y amargo": false,
+      "Todas las anteriores": true,
     },
   ),
   QuestionModel(
-    "¿Cuál es mi comida favorita?",
+    "¿Cuál es el lugar más frío de la tierra?",
     {
-      "Comida China": false,
-      "Comida rápida": false,
-      "Pastas": true,
-      "Antojitos mexicanos": false,
+      "El corazón de mi ex": false,
+      "Alaska": false,
+      "Antártida": true,
+      "Rusia": false,
     },
   ),
   QuestionModel(
-    "¿Cómo se llamó mi primer mascota?",
+    "¿Quién escribió La Odisea?",
     {
-      "Luna": false,
-      "Pipo": false,
-      "Hamtaro": true,
-      "Stwart": false,
+      "Paulo Cohelo": false,
+      "Homero Simpson": false,
+      "Homero": true,
+      "Mario Benedetti": false,
+    },
+  ),
+  QuestionModel(
+    "¿Cuál es el río más largo del mundo?",
+    {
+      "Nilo": false,
+      "Bravo": false,
+      "Amazonas": true,
+    },
+  ),
+  QuestionModel(
+    "¿Qué tipo de animal es la ballena?",
+    {
+      "Marino": false,
+      "Mamífero": false,
+      "Ambas, dos": true,
+    },
+  ),
+  QuestionModel(
+    "¿Cuándo acabó la II Guerra Mundial?",
+    {
+      "1939": false,
+      "1950": false,
+      "1945": true,
+      "1955": false,
+    },
+  ),
+  QuestionModel(
+    "¿Cómo se denomina el resultado de la multiplicación?",
+    {
+      "Resultado": false,
+      "Restante": false,
+      "Residuo": false,
+      "Producto": true,
+    },
+  ),
+  QuestionModel(
+    "¿Qué producto cultiva más Guatemala?",
+    {
+      "Café": true,
+      "Maíz": false,
+      "Azúcar": false,
+      "Algodón": false,
+    },
+  ),
+  QuestionModel(
+    "¿Qué significa FIFA?",
+    {
+      "Fúlbol Internacional": false,
+      "Fédération Internationale de Football Association": true,
+      "Federación Internacional de Fútbol Asociación​ ​": true,
+      "Fútbol Internacional For Action": false,
+    },
+  ),
+  QuestionModel(
+    "Si 50 es el 100%, ¿cuánto es el 90%?",
+    {
+      "45": true,
+      "55": false,
+      "75": false,
+      "35": false,
+    },
+  ),
+  QuestionModel(
+    "¿Qué país tiene forma de bota?",
+    {
+      "Suiza": false,
+      "Rusia": false,
+      "Italia": true,
+      "Monaco": false,
+    },
+  ),
+  QuestionModel(
+    "¿En qué lugar del cuerpo se produce la insulina?",
+    {
+      "Hígado": false,
+      "Peroné": false,
+      "Páncreas": true,
+      "Apendice": false,
+    },
+  ),
+  QuestionModel(
+    "¿En qué lugar del cuerpo se produce la insulina?",
+    {
+      "Blanco": false,
+      "Verde": true,
+      "Azúl": false,
+      "Amarillo": false,
+    },
+  ),
+  QuestionModel(
+    "¿Cómo se llama el himno nacional de Francia?",
+    {
+      "Lé Himné": false,
+      "Ratatouille": false,
+      "Marsellesa": true,
+      "Lé Himné de Francé": false,
+    },
+  ),
+  QuestionModel(
+    "¿Cuál es la ciudad de los rascacielos?",
+    {
+      "CDMX": false,
+      "Dubai": false,
+      "New York": true,
+      "Roma": false,
+    },
+  ),
+  QuestionModel(
+    "¿A qué país pertenecen los cariocas?",
+    {
+      "Brasil": true,
+      "Colombia": false,
+      "Venezuela": false,
+      "Perú": false,
+    },
+  ),
+  QuestionModel(
+    "¿Quién va a la cárcel: el imputado, el acusado, el condenado?",
+    {
+      "Acusado": false,
+      "Imputado": false,
+      "Condenado": true,
+      "Detenido": false,
+    },
+  ),
+  QuestionModel(
+    "¿Quién va a la cárcel: el imputado, el acusado, el condenado?",
+    {
+      "Acusado": false,
+      "Imputado": false,
+      "Condenado": true,
+      "Detenido": false,
+    },
+  ),
+  QuestionModel(
+    "¿Quién ganó el mundial de 2014?",
+    {
+      "Argentina": false,
+      "Brasil": false,
+      "Alemania": true,
+      "Reino Unido": false,
+    },
+  ),
+  QuestionModel(
+    "¿A qué país pertenece la ciudad de Varsovia?",
+    {
+      "Polonia": true,
+      "Moscú": false,
+      "España": false,
+      "Canada": false,
     },
   ),
 ];
